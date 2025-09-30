1ce1ffc948dc217f5f3986a20332d2e3f504490f
Thiago Santos <131814290+Thiagosantos758@users.noreply.github.com>
2025-09-30 08:19:12 -0300

    Create connect.php
    Criando o arquivo de conexão php do banco de dados aiven

---
+++ apiTest/connect.php
@@ -0,0 +1,22 @@
+<?php
+
+$uri = "mysql://avnadmin:AVNS_EoweUt9w0N3Fzraad65@meutcc-tcc2025.b.aivencloud.com:12566/defaultdb?ssl-mode=REQUIRED";
+
+$fields = parse_url($uri);
+
+// build the DSN including SSL settings
+$conn = "mysql:";
+$conn .= "host=" . $fields["host"];
+$conn .= ";port=" . $fields["port"];;
+$conn .= ";dbname=abcd";
+$conn .= ";sslmode=verify-ca;sslrootcert=ca.pem";
+
+try {
+  $db = new PDO($conn, $fields["user"], $fields["pass"]);
+
+  $stmt = $db->query("SELECT VERSION()");
+  print($stmt->fetch()[0]);
+} catch (Exception $e) {
+  echo "Error: " . $e->getMessage();
+}
+?>
